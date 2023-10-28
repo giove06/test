7c07b8067a3a434f75ece6f879fb2c3675769ef4
giove06 <ggiove@cerebotani.it>
2023-10-28 08:14:13 +0200

    funzionalitàSomma

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,8 @@
  class helloWorld{
-	 public static void main(String args[]){
-		 System.out.println("ciao mondo!");
-	 }
+	public static void main(String args[]){
+		//System.out.println("ciao mondo!");
+		int nu = 5;
+		int num = 3;
+		System.out.println(nu+ "+" +num + "=" + (nu+num));
+	}
  }
